901c998bc6758ccb1679fa247c127178df0502e7
hanul-cha <ckgks3014@naver.com>
2021-10-18 14:22:44 +0900

    clickEvent연결

--- src/component/Side.js
+++ src/component/Side.js
@@ -1,16 +1,44 @@
+import { useState } from "react"
+
 export default function Side(fullList) {
+    const [sideState, setSide] = useState(false)
+
 
     const list = fullList.list
+
+    const changeAdd = () => {
+        setSide(true)
+    }
+    
+    const changeSide = () => {
+        setSide(false)
+    }
     
 
-    return(
-        <>
-            <div className="side" style={{ width:300, height:603, paddingTop: 102, display:"block"}}>
-                <div style={{width: 300, height: 500, backgroundColor: "#f3f3f3"}}>
-                    <h2>{list.title}</h2>
-                    <p>{list.text}</p>
+    if(sideState){
+        return(
+            <>
+                <div className="addToDo" style={{ width:300, height:603, paddingTop: 102, display:"block"}}>
+                    <form className="toDoInner" style={{width: 300, height: 500}}>
+                        
+                        <button className="addBtn" onClick={changeSide}>적용</button>
+                    </form>
                 </div>
-            </div>
-        </>
-    )
+            </>
+        )
+    } else {
+        return(
+            <>
+                <div className="side" style={{ width:300, height:603, paddingTop: 102, display:"block"}}>
+                    <div className="sideInner" style={{width: 300, height: 500}}>
+                        <h2>{list.title}</h2>
+                        <p>{list.text}</p>
+                        <button className="toDoBtn" onClick={changeAdd}>일정추가</button>
+                    </div>
+                </div>
+            </>
+        )
+    }
+
+    
 }
