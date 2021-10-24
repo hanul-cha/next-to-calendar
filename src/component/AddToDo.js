c9eec69e4b637f92c133665f4c16373a3d834974
hanul-cha <ckgks3014@naver.com>
2021-10-24 22:50:31 +0900

    모듈 나누기(상위 컴포넌트 state변경하기)

--- src/component/AddToDo.js
+++ src/component/AddToDo.js
@@ -1,13 +1,45 @@
-import Side from "./Side";
+export default function AddToDo(prop) {
+    console.log(prop)
 
-export default function addToDo() {
+    const setSide = prop.setSide
 
+    const fullList = prop.fullList
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log(fullList.list.month)
+        fetch("http://localhost:5000/api/toDoList", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                year: fullList.list.year,
+                month: fullList.list.month,
+                day: Number(fullList.list.day),
+                list:{
+                    title: e.target[0].value,
+                    text: e.target[1].value
+                }
+            }),
+        })
+        .then((res) => res.json())
+        .then((res) => {
+            if(res === "ok") {
+                setSide(true)
+                window.location.replace("/")//새로고침해서 서버를 다시 키는걸로 타협함
+            }
+        })
+
+        /* setSide(false) */
+    }
 
     return(
         <>
             <div className="addToDo" style={{ width:300, height:603, paddingTop: 102, display:"block"}}>
-                <form className="toDoInner" style={{width: 300, height: 500}}>
-                    
+                <form className="toDoInner" style={{width: 300, height: 500}}  onSubmit={onSubmit}>
+                    <input placeholder="제목" required></input>
+                    <input placeholder="할일" required></input>
                     <button className="addBtn">적용</button>
                 </form>
             </div>
--- src/component/Side.js
+++ src/component/Side.js
@@ -1,72 +1,37 @@
 import { useRouter } from "next/dist/client/router"
 import { useState } from "react"
+import AddToDo from "./AddToDo";
 
 export default function Side(fullList) {
-    const [sideState, setSide] = useState(false)
+    const [sideState, setSide] = useState(true)
     const router = useRouter();
 
     const list = fullList.list.list
     
 
     const changeAdd = () => {
-        setSide(true)
+        setSide(false)
     }
     
-    const onSubmit = (e) => {
-        e.preventDefault();
-        console.log(fullList.list.month)
-        fetch("http://localhost:5000/api/toDoList", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                year: fullList.list.year,
-                month: fullList.list.month,
-                day: Number(fullList.list.day),
-                list:{
-                    title: e.target[0].value,
-                    text: e.target[1].value
-                }
-            }),
-        })
-        .then((res) => res.json())
-        .then((res) => {
-            if(res === "ok") {
-                setSide(false)
-                window.location.replace("/")//새로고침해서 서버를 다시 키는걸로 타협함
+    
+    
+    return (
+        <>
+            {sideState
+            ? 
+            <div className="side" style={{ width:300, height:603, paddingTop: 102, display:"block"}}>
+                <div className="sideInner" style={{width: 300, height: 500}}>
+                    <h2>{list.title}</h2>
+                    <p>{list.text}</p>
+                    <button className="toDoBtn" onClick={changeAdd}>일정추가</button>
+                </div>
+            </div>
+            :
+            <AddToDo fullList={fullList} setSide={setSide} />
             }
-        })
-
-        /* setSide(false) */
-    }
-    
-
-    if(sideState){
-        return(
-            <>
-                <div className="addToDo" style={{ width:300, height:603, paddingTop: 102, display:"block"}}>
-                    <form className="toDoInner" style={{width: 300, height: 500}}  onSubmit={onSubmit}>
-                        <input placeholder="제목" required></input>
-                        <input placeholder="할일" required></input>
-                        <button className="addBtn">적용</button>
-                    </form>
-                </div>
-            </>
-        )
-    } else {
-        return(
-            <>
-                <div className="side" style={{ width:300, height:603, paddingTop: 102, display:"block"}}>
-                    <div className="sideInner" style={{width: 300, height: 500}}>
-                        <h2>{list.title}</h2>
-                        <p>{list.text}</p>
-                        <button className="toDoBtn" onClick={changeAdd}>일정추가</button>
-                    </div>
-                </div>
-            </>
-        )
-    }
+        </>
+    )
+   
 
     
 }
